91013cb43ed3e16cacd5c31f53939ed25aebba64
LeafusBonesboro <108142251+LeafusBonesboro@users.noreply.github.com>
2025-02-07 07:16:07 -0500

    deploy ready

--- Backend/app.py
+++ Backend/app.py
@@ -12,4 +12,4 @@ def home():
     return render_template('home.html')  # Render the index.html template
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
